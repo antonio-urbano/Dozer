2efc6201560eb3e79bb9760439025ffbc48c22b0
antonio-urbano <antonio.urbano@mail.polimi.it>
2021-10-19 18:08:57 +0200

    integrated converter streams in DozerApplication

--- seraph.pipeline/src/main/java/application/DozerApplication.java
+++ seraph.pipeline/src/main/java/application/DozerApplication.java
@@ -2,6 +2,8 @@ package application;
 
 import cdc_converter.CdcDeleteRecord;
 import cdc_converter.CdcCreateRecord;
+import cdc_converter.JsonPG;
+import cdc_converter.ProcessorConverter;
 import custom_serdes.CdcSerde;
 import custom_serdes.CurrentAgentSerde;
 import custom_serdes.JsonDeserializer;
@@ -15,6 +17,7 @@ import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Produced;
 import org.apache.kafka.streams.state.Stores;
+import org.apache.tinkerpop.gremlin.structure.T;
 import processors.*;
 
 import java.util.Properties;
@@ -24,42 +27,34 @@ import java.util.concurrent.CountDownLatch;
 //todo javadoc
 public class DozerApplication {
 
-    static Properties getProcessorProperties(){
+    static Properties getStreamProperties(String applicationId, String bootstrapServer, String keySerde, Class<?> valueSerde){
         Properties props = new Properties();
 
-        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "dozer-processors-app"); //todo
-        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, applicationId); //todo
+        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
         props.putIfAbsent(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.putIfAbsent(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, CurrentAgentSerde.class);
+        props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, keySerde);
+        props.putIfAbsent(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, valueSerde);
         props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 
         return props;
     }
 
-    static Properties getStreamDeleteByEventProperties(){
-        Properties props = new Properties();
+    static void convertPgToCDC(final StreamsBuilder builder) {
 
-        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "dozer-delete-stream-event-app");    //todo
-        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.putIfAbsent(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.putIfAbsent(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-        props.putIfAbsent(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, CdcSerde.class);
-        props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        JsonSerializer<JsonPG> PGJsonSerializer = new JsonSerializer<>();
+        JsonDeserializer<JsonPG> PGJsonDeserializer = new JsonDeserializer<>(
+                JsonPG.class);
+        Serde<JsonPG> jsonSerde = Serdes.serdeFrom(PGJsonSerializer,
+                PGJsonDeserializer);
 
-        return props;
+
+        KStream<String, JsonPG> stream = builder.stream("JsonPG-topic",//todo
+                Consumed.with(Serdes.String(), jsonSerde));
+        stream.process(ProcessorConverter::new);
+
     }
 
-    static Properties getStreamDeleteByTimeProperties(){
-        Properties props = new Properties();
-
-        props.putIfAbsent(StreamsConfig.APPLICATION_ID_CONFIG, "dozer-delete-stream-time-app"); //todo
-        props.putIfAbsent(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.putIfAbsent(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-        props.putIfAbsent(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-
-        return props;
-    }
 
     static void produceDeleteRecordByTime(final StreamsBuilder builder, Long emit_time_range) {
 
@@ -111,30 +106,28 @@ public class DozerApplication {
     }
 
 
+    public static void main(final String[] args) {
 
-        public static void main(final String[] args) {
+        //todo flag
+        boolean tickerTimeFlag = true;      //true = ticker time, false = ticker event
+        boolean deleteProdTimeFlag = true;  //true = delete producer by time, false = delete producer by events
 
-            //todo flag
-            boolean tickerTimeFlag = true;      //true = ticker time, false = ticker event
-            boolean deleteProdTimeFlag = true;  //true = delete producer by time, false = delete producer by events
+        JsonSerializer<CurrentAgent> agentJsonSerializer = new JsonSerializer<>();
+        JsonDeserializer<CurrentAgent> agentJsonDeserializer = new JsonDeserializer<>(
+                CurrentAgent.class);
+        Serde<CurrentAgent> agentSerde = Serdes.serdeFrom(agentJsonSerializer,
+                agentJsonDeserializer);
 
-            JsonSerializer<CurrentAgent> agentJsonSerializer = new JsonSerializer<>();
-            JsonDeserializer<CurrentAgent> agentJsonDeserializer = new JsonDeserializer<>(
-                    CurrentAgent.class);
-            Serde<CurrentAgent> agentSerde = Serdes.serdeFrom(agentJsonSerializer,
-                    agentJsonDeserializer);
-
-            Serde<Long> longSerde = Serdes.Long();
+        Serde<Long> longSerde = Serdes.Long();
 
 
-            final Topology builder = new Topology();
-
-            final StreamsBuilder builder_deleteRecordByTime = new StreamsBuilder();
-            final Topology deleteByEvent_topology = new Topology();
-            if(deleteProdTimeFlag)
-                produceDeleteRecordByTime(builder_deleteRecordByTime, 300000L);     //todo
-            else
-                produceDeleteRecordByEvent(deleteByEvent_topology);
+        final Topology builder = new Topology();
+        final StreamsBuilder builder_deleteRecordByTime = new StreamsBuilder();
+        final Topology deleteByEvent_topology = new Topology();
+        if(deleteProdTimeFlag)
+            produceDeleteRecordByTime(builder_deleteRecordByTime, 300000L);     //todo
+        else
+            produceDeleteRecordByEvent(deleteByEvent_topology);
 
 
 
@@ -167,19 +160,32 @@ public class DozerApplication {
         builder.addSink("Sink", "processor-topic3","TickerProcessor", "TimeManagedProcessorDeletion", "TimeManagedProcessorInsertion", "CypherHandlerProcessor");
 
 
-        final KafkaStreams streams = new KafkaStreams(builder, getProcessorProperties());
-        final CountDownLatch latch = new CountDownLatch(2);
+        final CountDownLatch latch = new CountDownLatch(3);
+        final KafkaStreams streams = new KafkaStreams(builder, getStreamProperties(
+            "dozer-processors-app", "localhost:9092", //todo id and server
+            Serdes.String().getClass().getName(), CurrentAgentSerde.class));
         final KafkaStreams streamsDeleteProducer;
+
         if (deleteProdTimeFlag)
-            streamsDeleteProducer = new KafkaStreams(builder_deleteRecordByTime.build(), getStreamDeleteByTimeProperties());
+        streamsDeleteProducer = new KafkaStreams(builder_deleteRecordByTime.build(),
+                getStreamProperties("dozer-delete-stream-time-app", "localhost:9092",
+                        Serdes.String().getClass().getName(), CdcSerde.class));
         else
-            streamsDeleteProducer = new KafkaStreams(deleteByEvent_topology, getStreamDeleteByEventProperties());
+            streamsDeleteProducer = new KafkaStreams(deleteByEvent_topology,
+                    getStreamProperties("dozer-delete-stream-event-app", "localhost:9092",
+                            Serdes.String().getClass().getName(), CdcSerde.class));
 
+        final StreamsBuilder converterBuilder = new StreamsBuilder();
+        convertPgToCDC(converterBuilder);
+        final KafkaStreams streamsConverter = new KafkaStreams(converterBuilder.build(),
+                getStreamProperties("dozer-converter-processors-app", "localhost:9092", //todo
+                        Serdes.String().getClass().getName(), CdcSerde.class));
 
-        // attach shutdown handler to catch control-c
-        Runtime.getRuntime().addShutdownHook(new Thread("dozer-streams-shutdown-hook") {
+            // attach shutdown handler to catch control-c
+        Runtime.getRuntime().addShutdownHook(new Thread("dozer-streams-shutdown-hook") {//todo name
             @Override
             public void run() {
+                streamsConverter.close();
                 streams.close();
                 streamsDeleteProducer.close();
                 latch.countDown();
@@ -188,6 +194,7 @@ public class DozerApplication {
 
 
         try {
+            streamsConverter.start();
             streams.start();
             streamsDeleteProducer.start();
             latch.await();
