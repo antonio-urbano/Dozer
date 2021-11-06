32a5a527e855f5f817429a9c4ef5d0476be85f36
antonio-urbano <antonio.urbano@mail.polimi.it>
2021-11-06 15:18:42 +0100

    changed max poll

--- seraph.pipeline/src/main/java/config/KafkaConfigProperties.java
+++ seraph.pipeline/src/main/java/config/KafkaConfigProperties.java
@@ -51,7 +51,7 @@ public class KafkaConfigProperties {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, DozerConfig.getSeraphQuery().getQueryID() +"_"+applicationId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
-        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);  //todo
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 100);  //todo
         props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, deserializerClass);
 
         return props;
